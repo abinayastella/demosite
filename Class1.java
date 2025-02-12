8da82d3e42f73d195413e23084761191c43d1ae4
abinayastella <abinayastella@iattechologies.com>
2025-02-12 16:45:02 +0530

    commit last mg

---
+++ Class1.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Class1 {
+	public static void main(String[] args) {
+		int i=5;
+		if (i%2==1) {
+			System.out.println("add number");
+		}
+		else {
+			System.out.println("false");
+		}
+		
+		}
+}
